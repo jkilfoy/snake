5d49b41a0585a9966f064f478e737012c9e90d90
jkilfoy <jordankilfoy@gmail.com>
2022-03-23 09:04:04 -0400

    add snake event handler to Game

--- src/main/java/com/game/gamewarmup/Game.java
+++ src/main/java/com/game/gamewarmup/Game.java
@@ -8,18 +8,22 @@ import javafx.scene.input.KeyEvent;
 public class Game extends Canvas {
 
     public static final int SIZE = 16;
-    public static final int LENGTH = 512;
+    public static final int WORLD_SIZE = 512;
     public static final int BORDER_SIZE = 16;
 
     GraphicsContext context;
     World world;
 
     public Game() {
-        super(LENGTH, LENGTH);
-        world = new World(LENGTH, SIZE, BORDER_SIZE);
+        super(WORLD_SIZE, WORLD_SIZE);
+        world = new World(WORLD_SIZE, WORLD_SIZE, BORDER_SIZE);
+        context = getGraphicsContext2D();
+
+        // add event handler
         addEventHandler(KeyEvent.KEY_PRESSED, world.getSnake());
         setFocusTraversable(true);
-        context = getGraphicsContext2D();
+
+        // run the game
         run();
     }
 
@@ -35,16 +39,11 @@ public class Game extends Canvas {
     }
 
     private void update() {
-        // move snake
-
-        // detect collision
-
-        // resolve collisions
+        world.update();
     }
 
     private void render() {
         world.render(context);
-
     }
 
 }
